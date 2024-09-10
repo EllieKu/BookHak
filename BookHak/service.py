b9f247c3ac65f9da15877358323286cbbdd14563
Ellie <Ellie.Ku@want-want.com>
2024-09-10 16:37:27 +0800

    refactor

--- BookHak/service.py
+++ BookHak/service.py
@@ -1,15 +1,26 @@
 from BookHak.crawler import Books, Readmoo
 from BookHak.crud import find_book_by_title, create_book
+from BookHak.utils.log import logger
 
 
 def get_reviews_by_title(book_title: str):
-    book = find_book_by_title(book_title)
-    if book:
-        return book["reviews"]
+    logger.info(f"Start to get reviews of book 『{book_title}』")
+    try:
+        book = find_book_by_title(book_title)
+        if book:
+            return book["reviews"]
 
-    reviews = Readmoo(book_title).get_reviews_pipeline()
-    reviews += Books(book_title).get_reviews_pipeline()
+        reviews = []
+        reviews_in_readmoo = Readmoo(book_title).get_reviews_pipeline()
+        logger.info("Get reviews in Readmoo succeed!")
+        reviews += reviews_in_readmoo
 
-    create_book(book_title, reviews)
+        reviews_in_books = Books(book_title).get_reviews_pipeline()
+        logger.info("Get reviews in Books succeed!")
+        reviews += reviews_in_books
 
-    return reviews
+        create_book(book_title, reviews)
+
+        return reviews
+    except Exception as e:
+        logger.error(f"Fail to get reviews: {e}")
